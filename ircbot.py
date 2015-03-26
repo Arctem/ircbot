82208fbf94419def35dfc0776add5d3528e05fcf
Russell White <rarctem@gmail.com>
2015-03-26 14:42:35 -0500

    Move handling of messages to a proper spot.

--- ircbot.py
+++ ircbot.py
@@ -17,75 +17,64 @@ class IRCBot:
 
     def sendmsg(self, msg):
         if not msg:
-            return False:
+            return False
+        print('Sending: {}'.format(msg))
         if msg[-1] != '\n':
             msg += '\n'
         self.sock.send(msg.encode())
         return True
 
     def connect(self, host, port, rooms = None):
-        if sock:
+        if self.sock:
             return False
 
         self.sock=socket.socket()
         self.sock.connect((host, port))
+        self.rooms = rooms or []
          
-        self.sendmsg("NICK {}".format(nick), "UTF-8")
-        self.sendmsg("USER {} {} bla :{}".format(nick, host, realname))
+        self.sendmsg("NICK {}".format(self.nick))#, "UTF-8")
+        self.sendmsg("USER {} {} bla :{}".format(self.nick, host, self.realname))
 
-        self.sendmsg("JOIN #glory");
-         
-        while 1:
-            readbuffer = readbuffer+s.recv(1024).decode("UTF-8")
-            temp = str.split(readbuffer, "\n")
-            readbuffer=temp.pop()
-         
-            for line in temp:
-                line = str.rstrip(line)
-                line = str.split(line)
-         
-                if(line[0] == "PING"):
-                    s.send(bytes("PONG %s\r\n" % line[1], "UTF-8"))
-                if(line[1] == "PRIVMSG"):
-                    sender = ""
-                    for char in line[0]:
-                        if(char == "!"):
+    def process(self):
+        if not self.sock:
+            return False
+
+        inputs = [self.sock, sys.stdin]
+        while True:
+            in_ready, out_ready, except_ready = select.select(inputs, [], [])
+
+            for item in in_ready:
+                if item == sys.stdin:
+                    self.sendmsg(item.readline().strip())
+                elif item == self.sock:
+                    recv = item.recv(4096).decode()
+                    for recv in recv.split('\n'):
+                        if recv == None:
+                            print('Remote socket {} closed.'.format(self.sock))
                             break
-                        if(char != ":"):
-                            sender += char 
-                    size = len(line)
-                    i = 3
-                    message = ""
-                    while(i < size): 
-                        message += line[i] + " "
-                        i = i + 1
-                    message.lstrip(":")
-                    s.send(bytes("PRIVMSG %s %s \r\n" % (sender, message), "UTF-8"))
-                for index, i in enumerate(line):
-                    print(line[index])
+                        if len(recv) == 0:
+                            continue
+
+                        prefix, cmd, args = ircutil.parsemsg(recv)
+                        if cmd == 'PING':
+                            self.sendmsg('PONG {}'.format(args[0]))
+                        elif cmd == 'MODE':
+                            if prefix == self.nick:
+                                for room in self.rooms:
+                                    self.sendmsg("JOIN {}".format(room))
+                            else:
+                                print('Unhandled: {}'.format(recv))
+                        else:
+                            print('Unrecognized command {}: {}'.format(cmd, recv))
+
+                else:
+                    print('Something broke: {}'.format(item))
 
 
 def main():
-    #bot = IRCBot("ircbot", "IRC Bot")
-    #bot.connect("irc.arctem.com", 6667)
-    sock = socket.socket()
-    sock.connect(('irc.arctem.com', 6667))
-    sock.send("NICK ircbot\n".encode())
-    sock.send("USER ircbot irc.arctem.com bla :IRC Bot\n".encode())
-    sock.send("JOIN #glory\n".encode())
-
-    while True:
-        input_ready, output_ready, except_ready = select.select([sock, sys.stdin], [], [])
-
-        for item in input_ready:
-            if item == sys.stdin:
-                sock.send("{}\n".format(item.readline().strip()).encode())
-            else:
-                recv = item.recv(1024).decode()
-                print(recv)
-                if(recv.startswith("PING")):
-                    reply = recv[5:]
-                    sock.send("PONG {}\n".format(reply).encode())
+    bot = IRCBot("ircbot", "IRC Bot")
+    bot.connect("irc.arctem.com", 6667, ['#glory'])
+    bot.process()
 
 
 if __name__ == '__main__':
