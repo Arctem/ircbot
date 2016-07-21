76a87af1a7740d740a2ac9ed95a670a5be735ada
Russell White <rarctem@gmail.com>
2016-07-21 15:22:54 -0400

    Bump version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import ircbot
 
 setup(name='ircbot',
-    version='0.2',
+    version='0.3',
     description='A simple library for making IRC bots.',
     url='http://github.com/Arctem/ircbot/',
     author='Russell White',
